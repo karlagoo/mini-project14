be422096d92e269ad407cddd01a59e0c14912095
Karla Goo <karla_goo@yahoo.com>
2021-09-11 14:56:26 -0500

    fixed single project route

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,15 +1,32 @@
 const router = require('express').Router();
-const { User }= require('../models');
+const { User , Project}= require('../models');
 
 router.get('/', async (req, res)=> {
-    const userData = await User.findAll();
-    console.log(userData)
+    const projectData = await Project.findAll();
+    console.log(projectData)
 
-    const users = userData.map((user) =>
-    user.get({ plain: true})
+    const projects = projectData.map((project) =>
+    project.get({ plain: true})
     );
-    console.log(users);
-    res.render('homepage', {users});
+    console.log(projects);
+    res.render('homepage', {projects});
 });
+
+router.get('/project/:id', async (req, res) =>{
+    try{
+      const projectData = await Project.findByPk(req.params.id,{
+        where:{
+          id: req.params.id
+        }
+      });
+      const project = projectData.get({ plain: true});
+      console.log(project);
+      res.render('singleproject', {project})
+      
+    }
+    catch(err){
+      res.status(500).json(err);
+    }
+  })
 
 module.exports = router;
